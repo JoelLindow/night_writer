68fbd92526bcbe055c39659c970e53703958d614
JoelLindow <joellindow@gmail.com>
2017-05-13 08:43:10 -0600

    enhanced translate to translate

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -6,19 +6,23 @@ class NightWriter
 
   def initialize(text_in = read_file)
     @text_in = text_in
-
+    @braile_code = translate_to_braile_code
+    # binding.pry
   end
 
   def read_file
     f = File.read(ARGV[0]).chomp
   end
 
-  def translate
+  def translate_to_braile_code
     new_braille = []
     braille_key = BrailleLibrary.new
     new_braille << braille_key.braille[text_in]
-    #add code to split up characters
     new_braille
+  end
+
+  def braile_to_three_line
+    
   end
 
 end
