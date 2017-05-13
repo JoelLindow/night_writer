30da9638492d1dbd1efdf11fc21295166d0b1679
JoelLindow <joellindow@gmail.com>
2017-05-13 12:06:33 -0600

    working 3 line test

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,12 +2,14 @@ require './lib/braille_library'
 require 'pry'
 
 class NightWriter
-  attr_reader :text_in, :braille_code
+  attr_reader :text_in, :braille_code, :line_one, :line_two, :line_three
 
-  def initialize( text_in = read_file)
+  def initialize(text_in = read_file)
     @text_in = text_in
     @braille_code = translate_to_braille_code
-
+    @line_one = ""
+    @line_two = ""
+    @line_three = ""
   end
 
   def read_file
@@ -25,13 +27,22 @@ class NightWriter
 
   def braile_to_three_line
     braille_code.each do |braille_letter|
-      puts braille_letter[1]
-      puts braille_letter[2]
-      puts braille_letter[3]
+      line_one << braille_letter[0]
+      line_two << braille_letter[1]
+      line_three << braille_letter[2]
+      # binding.pry
     end
+  end
+
+  def write_text_to_file
+    # File.open()
   end
 
 end
 
-input = gets.chomp
-nw = NightWriter.new(input)
+# input = gets.chomp
+# nw = NightWriter.new(input)
+# nw.braile_to_three_line
+# p nw.line_one
+# p nw.line_two
+# p nw.line_three
