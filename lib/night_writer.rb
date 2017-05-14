bb4259122f799348fbda9c738b4f6bbc9c462488
JoelLindow <joellindow@gmail.com>
2017-05-14 10:30:04 -0600

    3 line conversion broken out. uppdercase and punctuation symbols work. tests pass

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,14 +2,17 @@ require './lib/braille_library'
 require 'pry'
 
 class NightWriter
-  attr_reader :text_in, :braille_code, :line_one, :line_two, :line_three
+  attr_reader :text_in, :braille_code
 
   def initialize(text_in = read_file)
     @text_in = text_in
     @braille_code = translate_to_braille_code
-    @line_one = ""
-    @line_two = ""
-    @line_three = ""
+  end
+
+  def convert
+      line_one
+      line_two
+      line_three
   end
 
   def read_file
@@ -25,23 +28,40 @@ class NightWriter
     converted_to_braille_string
   end
 
-  def braile_to_three_line
-    braille_code.each do |braille_letter|
+  def line_one
+    line_one = ""
+    braille_code.map do |braille_letter|
       line_one << braille_letter[0]
-      line_two << braille_letter[1]
-      line_three << braille_letter[2]
     end
+    line_one
   end
 
+  def line_two
+    line_two = ""
+    braille_code.map do |braille_letter|
+      line_two << braille_letter[1]
+    end
+    line_two
+  end
+
+  def line_three
+    line_three = ""
+    braille_code.map do |braille_letter|
+      line_three << braille_letter[2]
+    end
+    line_three
+  end
+
+
   def write_text_to_file
-    # File.open()
+    File.open()
   end
 
 end
 
 # file = File.read(ARGV[0]).chomp
 # nw = NightWriter.new(file)
-# nw.braile_to_three_line
+# nw.convert
 # p nw.line_one
 # p nw.line_two
 # p nw.line_three
