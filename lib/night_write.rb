dbe5fe59926b387c21e33d7b4e8da67fe3f02615
JoelLindow <joellindow@gmail.com>
2017-05-15 14:13:45 -0600

    fixed broken refactor or runner file

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -5,4 +5,4 @@ to_braille = Translate.new(File.read(ARGV[0]).chomp)
 to_braille.convert
 
 output = File.open(ARGV[1], 'w+')
-output.write(magic.write_text_to_file)
+output.write(to_braille.write_text_to_file)
