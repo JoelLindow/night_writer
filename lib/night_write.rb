77ce2c2003c37f3c85dd569fa2d6df49e15ab8c3
JoelLindow <joellindow@gmail.com>
2017-05-15 14:04:12 -0600

    refactor line one line two line three

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -4,4 +4,5 @@ magic = Translate.new(File.read(ARGV[0]).chomp)
 
 magic.convert
 
-File.open(ARGV[1], 'w+') { |file| file.write(magic.write_text_to_file) }
+output = File.open(ARGV[1], 'w+')
+output.write(magic.write_text_to_file)
